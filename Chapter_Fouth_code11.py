20fdf314dbc3ac4caeceeab8559161b4e17122bc
ic1396 <redsirius@foxmail.com>
2018-06-13 18:35:38 +0800

    第四章  示例代码11 Variable 的用法

---
+++ Chapter_Fouth_code11.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 第四章  示例代码11 Variable 的用法
+
+import tensorflow as tf
+
+var1 = tf.Variable(1.0, name = 'firstvar')
+print("var1: ", var1.name)
+var1 = tf.Variable(2.0, name = 'firstvar')
+print("var1: ", var1.name)
+var2 = tf.Variable(3.0)
+print("var2: ", var2.name)
+var2 = tf.Variable(4.0)
+print("var2: ", var2.name)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print("var1 = ", var1.eval())
+    print("var2 = ", var2.eval())
