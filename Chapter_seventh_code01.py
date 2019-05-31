f5ea4183c3612910c065e71815ffba1d90b00c4a
“ic1396 <redsirius@foxmail.com>
2019-06-01 01:34:13 +0800

    第七章 示例代码01 线性逻辑回归 完成模型训练。

--- Chapter_seventh_code01.py
+++ Chapter_seventh_code01.py
@@ -5,7 +5,9 @@
 import numpy as np
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
+# 生成模拟数据
 def generate (sample_size, mean, cov, diff, regression):
     num_classes = 2
     samples_per_class = int(sample_size / 2)
@@ -19,13 +21,15 @@ def generate (sample_size, mean, cov, diff, regression):
 
         X0 = np.concatenate((X0, X1))
         Y0 = np.concatenate((Y0, Y1))
-    if regression == False:
+    if regression == False:   # one-hot 编码，将 0 转换成 10
         class_ind = [Y == class_number for class_number in range(num_classes)]
         Y = np.asarray(np.hstack(class_ind), dtype=np.float32)
     X, Y = shuffle(X0, Y0)
 
     return X, Y
 
+# 数据可视化
+input_dim = 2
 np.random.seed(10)
 num_classes = 2
 mean = np.random.randn(num_classes)
@@ -37,3 +41,56 @@ plt.xlabel("Scaled age (in yrs)")
 plt.ylabel("Tumor size (in cm)")
 plt.show()
 lab_dim = 1
+
+# 定义学习参数和模型
+input_features = tf.placeholder(tf.float32, [None, input_dim])
+input_lables = tf.placeholder(tf.float32, [None, lab_dim])
+# 定义学习参数
+W = tf.Variable(tf.random_normal([input_dim, lab_dim]), name="weight")
+b = tf.Variable(tf.zeros([lab_dim]), name="bias")
+
+output = tf.nn.sigmoid(tf.matmul(input_features, W) + b)
+cross_entropy = -(input_lables * tf.log(output) + (1 - input_lables) * tf.log(1 - output))
+ser = tf.square(input_lables - output)
+loss = tf.reduce_mean(cross_entropy)
+err = tf.reduce_mean(ser)
+optimizer = tf.train.AdamOptimizer(0.04)   #收敛快，可动态调节梯度
+train = optimizer.minimize(loss)
+
+# 设置参数训练
+maxEpochs = 50
+minibatchSize = 25
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    
+    for epoch in range(maxEpochs):
+        sumerr = 0
+        for i in range(np.int32(len(Y) / minibatchSize)):
+            x1 = X[i * minibatchSize: (i + 1) * minibatchSize,:]
+            y1 = np.reshape(Y[i * minibatchSize: (i + 1) * minibatchSize], [-1, 1])
+            tf.reshape(y1, [-1, 1])
+            _, lossval, outputval, errval = sess.run([train, loss, output, err], 
+                              feed_dict={input_features: x1, input_lables: y1})
+            sumerr = sumerr + errval
+            
+        print("Epoch:", '%04d' % (epoch + 1), "cost =", "{:.9f}".format(lossval),\
+              "err =", sumerr / minibatchSize)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
